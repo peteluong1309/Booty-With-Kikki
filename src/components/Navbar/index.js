1c2cbf68eac8ebe11305df80e28acbf11e9fad2f
tuan.luong <tuan.luong@terralogic.com>
2021-03-10 17:45:36 +0700

    update

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { AppBar, Link, Toolbar, Typography } from "@material-ui/core";
+import styles from './styles.module.scss'
 
 const NavBar = (props) => {
 //   const classes = useStyles();
   return (
-    <AppBar>
-      <Toolbar>
-        <Typography>
-          <Link href="/workout-plan" >Workout Plan</Link>
+    <AppBar className={styles.base}>
+      <Toolbar className={styles.tyson_toolbar}>
+        <Typography className={styles.tyson_text}>
+          <Link className={styles.tyson_link} href="/workout-plan" >Workout Plan</Link>
         </Typography>
-        <Typography>
-          <Link href="/meal-plan">Meal Plan</Link>
+        <Typography className={styles.tyson_text}>
+          <Link className={styles.tyson_link} href="/meal-plan">Meal Plan</Link>
         </Typography>
       </Toolbar>
     </AppBar>
