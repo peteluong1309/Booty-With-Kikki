da916277c9f47f14151724db184ff8b66fef8e2b
tuan.luong <tuan.luong@terralogic.com>
2021-03-12 17:03:17 +0700

    update

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,23 +1,50 @@
 import React from "react";
-import { AppBar, Link, Toolbar, Typography } from "@material-ui/core";
-import styles from './styles.module.scss'
+import {
+  AppBar,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Toolbar,
+} from "@material-ui/core";
+import styles from "./styles.module.scss";
+import { AccountCircle, HomeRounded, ShoppingCart } from "@material-ui/icons";
 
 const navItem = [
-  {value: 'Workout Plan', path: '/workout-plan'},
-  {value: 'Meal Plan', path: '/meal-plan'},
-]
+  { value: "Workout Plan", path: "/workout-plan" },
+  { value: "Meal Plan", path: "/meal-plan" },
+  { value: "Contact", path: "/contact" },
+  { value: "About", path: "/about" },
+];
 
 const NavBar = (props) => {
-//   const classes = useStyles();
+  //   const classes = useStyles();
   return (
-    <AppBar className={styles.base}>
+    <AppBar className={styles.root}>
       <Toolbar className={styles.tyson_toolbar}>
-        <Typography className={styles.tyson_text}>
-          <Link className={styles.tyson_link} href="/workout-plan" >Workout Plan</Link>
-        </Typography>
-        <Typography className={styles.tyson_text}>
-          <Link className={styles.tyson_link} href="/meal-plan">Meal Plan</Link>
-        </Typography>
+        <IconButton className={styles.tyson_toolbar_icon}>
+          <HomeRounded />
+        </IconButton>
+        <List className={styles.tyson_toolbar_list}>
+          {navItem.map((item) => {
+            return (
+              <ListItem className={styles.listItemLink}>
+                <a href={item.path} className={styles.listItemLink_item}>
+                  <ListItemText
+                    className={styles.listItemLink_item_text}
+                    primary={item.value}
+                  />
+                </a>
+              </ListItem>
+            );
+          })}
+        </List>
+        <IconButton>
+          <ShoppingCart />          
+        </IconButton>  
+        <IconButton>
+          <AccountCircle />          
+        </IconButton>  
       </Toolbar>
     </AppBar>
   );
