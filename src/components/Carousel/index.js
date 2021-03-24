edbf19044f37e3687159f50203593c970b900121
peteluong1309 <luongdoquangtuan@gmail.com>
2021-03-24 16:58:57 +0700

    update

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const Carousel = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className = >
+      <div class="mySlides fade">
+        <div class="">1 / 3</div>
+        <img src="fav" alt='this is test' />
+        <div class="text">Caption Text</div>
+      </div>
+
+      <div class="mySlides fade">
+        <div class="">2 / 3</div>
+        <img src="fav" alt='this is test' />
+        <div class="text">Caption Two</div>
+      </div>
+
+      <div class="mySlides fade">
+        <div class="">3 / 3</div>
+        <img src="fav" alt='this is test' />
+        <div class="text">Caption Three</div>
+      </div>
+
+      <a class="prev" onclick="plusSlides(-1)">
+        &#10094;
+      </a>
+      <a class="next" onclick="plusSlides(1)">
+        &#10095;
+      </a>
+    </div>
+  );
+};
 
 export default Carousel;
