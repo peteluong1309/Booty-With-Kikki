f509f8fe0e7acd793ed37dd0337bc338266b622c
tuan.luong <tuan.luong@terralogic.com>
2021-05-21 01:16:36 +0700

    add new file: fake api and adminLayout.jsx

---
+++ src/layout/adminLayout.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class adminLayout extends Component {
+    render() {
+        return (
+            <div className='container'>
+                
+            </div>
+        )
+    }
+}
